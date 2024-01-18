91468bbe7b663ae7294e11dc02f214180eec5ffa
doanhtu07 <doanhtu07@gmail.com>
2024-01-18 00:47:49 -0600

    Temporarily empty state

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,12 +5,15 @@ import Navbar from "./_components/Navbar"
 import MainContent from "./_components/MainContent"
 
 export default async function Home() {
-  const songsResult = await fetch(`${process.env.BASE_URL}/api/songs`, {
-    next: {
-      tags: ["songs"],
-      revalidate: 4, // 4 seconds - interval 5 seconds revalidate in MainContent
-    },
-  })
+  const songsResult = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/songs`,
+    {
+      next: {
+        tags: ["songs"],
+        revalidate: 4, // 4 seconds - interval 5 seconds revalidate in MainContent
+      },
+    }
+  )
 
   const songsData = (await songsResult.json()) as { songs: Song[] }
   const songs = songsData.songs
